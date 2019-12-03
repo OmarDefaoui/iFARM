3f7a73f3ce89428ff1c3c596de6068a6700a4f5e
Omar Defaoui <omardefaoui@gmail.com>
2019-12-03 14:42:37 +0100

    Add weather activity

--- app/src/main/java/com/example/ifarm/menuActivity.java
+++ app/src/main/java/com/example/ifarm/menuActivity.java
@@ -6,10 +6,16 @@ import androidx.cardview.widget.CardView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+
+import com.example.ifarm.weather.activities.MainActivity;
 
 public class menuActivity extends AppCompatActivity implements View.OnClickListener {
+
     private CardView moveToStatistics, moveToFields, moveToWeather, moveToUpdate,
             moveToTips, moveToSettings;
+
+    private ImageView iv_record;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +29,7 @@ public class menuActivity extends AppCompatActivity implements View.OnClickListe
         moveToUpdate = (CardView) findViewById(R.id.update);
         moveToTips = (CardView) findViewById(R.id.tips);
         moveToSettings = (CardView) findViewById(R.id.settings);
+        iv_record = findViewById(R.id.iv_record);
 
 
         //ADDING EVENTS LISTENER TO THE CARDS
@@ -32,7 +39,7 @@ public class menuActivity extends AppCompatActivity implements View.OnClickListe
         moveToUpdate.setOnClickListener(this);
         moveToTips.setOnClickListener(this);
         moveToSettings.setOnClickListener(this);
-
+        iv_record.setOnClickListener(this);
 
     }
 
@@ -49,13 +56,20 @@ public class menuActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(intent);
                 break;
             case R.id.weather:
-                intent = new Intent(this, weatherActivity.class);
+                intent = new Intent(this, MainActivity.class);
                 startActivity(intent);
                 break;
             case R.id.settings:
                 intent = new Intent(this, SettingsActivity.class);
                 startActivity(intent);
                 break;
+            case R.id.iv_record:
+                recordAudio();
+                break;
         }
+    }
+
+    private void recordAudio() {
+        //TODO: function to interact with voice control
     }
 }
